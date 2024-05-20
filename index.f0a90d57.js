de3ee26a700c82cc399d13d90cc0c3a3b833fd7d
MariaKhmel <94320565+MariaKhmel@users.noreply.github.com>
2024-05-20 00:53:54 +0000

    Deploying to gh-pages from @ MariaKhmel/cracking-js@8b216c7cbc81fbd2d6fade4ecac60aadd6e82475 🚀

---
+++ index.f0a90d57.js
@@ -0,0 +1,2 @@
+!function(){var e=new Storage(["🍎","🍋","🍇","🍑"]),t=e.getItems();console.table(t),e.addItem("🍌"),console.table(e.items),e.removeItem("🍋"),console.table(e.items)}();
+//# sourceMappingURL=index.f0a90d57.js.map
---
+++ index.fd449add.js
@@ -0,0 +1,2 @@
+const e=new Storage(["🍎","🍋","🍇","🍑"]),t=e.getItems();console.table(t),e.addItem("🍌"),console.table(e.items),e.removeItem("🍋"),console.table(e.items);
+//# sourceMappingURL=index.fd449add.js.map
